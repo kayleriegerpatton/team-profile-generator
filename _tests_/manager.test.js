d345a5284804ccb948a4bef3fb680ae7d12a53de
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-11-17 16:14:52 -0600

    Employee class structure

---
+++ _tests_/manager.test.js
@@ -0,0 +1,20 @@
+// classes
+class Employee {
+  constructor(name, id, email) {
+    this.name = name;
+    this.id = id;
+    this.email = email;
+  }
+  getName() {}
+  getId() {}
+  getEmail() {}
+  getRole() {
+    // return Employee
+  }
+}
+
+class Manager {}
+
+class Engineer {}
+
+class Intern {}
