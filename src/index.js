3e0b4b8bf43799e847713aed5613e03a36f45f57
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-11-18 21:07:10 -0600

    subclasses in correct files

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
+// imports
+const employee = require("../lib/employee");
+const engineer = require("../lib/engineer");
+const manager = require("../lib/manager");
+const intern = require("../lib/intern");
 
+// export
+module.exports = {};
