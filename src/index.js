57a6bc88cb11f2a20a1931f33b8bd10b0ec9b20a
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-11-19 22:14:45 -0600

    basic test structures

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 // imports
-const employee = require("../lib/Employee");
-const engineer = require("../lib/Engineer");
-const manager = require("../lib/Manager");
-const intern = require("../lib/Intern");
+const Employee = require("../lib/Employee");
+const Engineer = require("../lib/Engineer");
+const Manager = require("../lib/Manager");
+const Intern = require("../lib/Intern");
 
 const inquirer = require("inquirer");
 const fs = require("fs");
