55959ef1c5952a6c69872d5582f4e6e558a5e5cc
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-11-19 19:57:29 -0600

    finish html styling & classes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const employee = require("../lib/employee");
 const engineer = require("../lib/engineer");
 const manager = require("../lib/manager");
 const intern = require("../lib/intern");
+const inquirer = require("inquirer");
 
 // export
 module.exports = {};
