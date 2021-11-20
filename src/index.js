e91a5f45d45342147f20658cb5417eb6dc1b0d03
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-11-20 11:20:18 -0600

    manager test issue

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
 // imports
-const Employee = require("../lib/Employee");
-const Engineer = require("../lib/Engineer");
-const Manager = require("../lib/Manager");
-const Intern = require("../lib/Intern");
+const { Employee, Engineer, Manager, Intern } = require("./lib");
 
 const inquirer = require("inquirer");
 const fs = require("fs");
-
-// export index?
-// module.exports = {};
