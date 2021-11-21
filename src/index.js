89390ccd5a5806e3dddc954b2c303c3774e8bcbb
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-11-20 21:06:26 -0600

    initial manager questions

--- src/index.js
+++ src/index.js
@@ -3,3 +3,37 @@ const { Employee, Engineer, Manager, Intern } = require("./lib");
 
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+const managerQuestions = [
+  {
+    type: "input",
+    name: "managerName",
+    message: "Enter your name:",
+  },
+  {
+    type: "input",
+    name: "managerId",
+    message: "Enter your ID number:",
+  },
+  {
+    type: "input",
+    name: "managerEmail",
+    message: "Enter your email address:",
+  },
+  {
+    type: "list",
+    name: "newEmployeeType",
+    message: "Which type of employee do you want to enter?:",
+    choices: [
+      { name: "Engineer", value: "engineer" },
+      { name: "Intern", value: "Intern" },
+    ],
+  },
+];
+
+const start = async () => {
+  // prompt initial Manager questions
+  const { answers } = await inquirer.prompt(managerQuestions);
+};
+
+start();
