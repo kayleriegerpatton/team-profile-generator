f2948a3b36a646f7f07cbbae1c286e5d0c0310b7
Kayle Rieger Patton <kayle.patton22@gmail.com>
2021-11-20 22:14:45 -0600

    question arrays

--- src/index.js
+++ src/index.js
@@ -42,15 +42,68 @@ const managerQuestions = [
     message: "Do you want to create an employee?",
     default: false,
   },
-  //   {
-  //     type: "list",
-  //     name: "newEmployeeType",
-  //     message: "Which type of employee do you want to enter?:",
-  //     choices: [
-  //       { name: "Engineer", value: "engineer" },
-  //       { name: "Intern", value: "Intern" },
-  //     ],
-  //   },
+];
+
+const employeeTypeQuestion = [
+  {
+    type: "list",
+    name: "employeeType",
+    message: "Which type of employee do you want to enter?:",
+    choices: [
+      { name: "Engineer", value: "engineer" },
+      { name: "Intern", value: "Intern" },
+    ],
+  },
+];
+
+const engineerQuestions = [
+  {
+    type: "input",
+    name: "engineerName",
+    message: "Enter your name:",
+  },
+  {
+    type: "number",
+    name: "engineerId",
+    message: "Enter your ID number:",
+    // validate: validateNumber(),
+    // if NaN, throw error message & repeat prompt???
+  },
+  {
+    type: "input",
+    name: "engineerEmail",
+    message: "Enter your email address:",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "Enter your GitHub username:",
+  },
+];
+
+const internQuestions = [
+  {
+    type: "input",
+    name: "internName",
+    message: "Enter your name:",
+  },
+  {
+    type: "number",
+    name: "internId",
+    message: "Enter your ID number:",
+    // validate: validateNumber(),
+    // if NaN, throw error message & repeat prompt???
+  },
+  {
+    type: "input",
+    name: "internEmail",
+    message: "Enter your email address:",
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "Enter your school name:",
+  },
 ];
 
 const start = async () => {
